db22233f413aeac316ec8708df644cb508a9ba66
Jacobo Narváez <jacobo.narvaez@ucuenca.edu.ec>
2022-12-27 21:51:29 -0600

    Reglas, imagenes y celdas ya ingresadas
    Array con opciones ya ingresadas

--- sketch.js
+++ sketch.js
@@ -1,7 +1,114 @@
+const celdas = []; // 4x4
+const RETICULA = 4
+
+const azulejos = [];
+const NA = 11; //numero de azulejos
+
+const reglas = [
+  //reglas de los bordes de cada azulejo
+  {
+    //tile 0
+    UP: 0,
+    RIGHT: 1,
+    DOWN: 1,
+    LEFT: 1,
+  },
+  {
+    //tile 1
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 1,
+    LEFT: 1,
+  },
+  {
+    //tile 2
+    UP: 1,
+    RIGHT: 1,
+    DOWN: 0,
+    LEFT: 1,
+  },
+  {
+    //tile 3
+    UP: 1,
+    RIGHT: 1,
+    DOWN: 1,
+    LEFT: 0,
+  },
+  {
+    //tile 4
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 0,
+  },
+  {
+    //tile 5
+    UP: 1,
+    RIGHT: 1,
+    DOWN: 1,
+    LEFT: 1,
+  },
+  {
+    //tile 6
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 0,
+  },
+  {
+    //tile 7
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 1,
+    LEFT: 1,
+  },
+  {
+    //tile 8
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 1,
+  },
+  {
+    //tile 9
+    UP: 0,
+    RIGHT: 1,
+    DOWN: 1,
+    LEFT: 0,
+  },
+  {
+    //tile 10
+    UP: 1,
+    RIGHT: 1,
+    DOWN: 0,
+    LEFT: 0,
+  },
+
+]
+
+function preload() {
+  for (let i = 0; i < NA; i++) {
+    azulejos[i] = loadImage(`tiles/tile${i}.png`);
+  }
+
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(1080, 1080);
+  let opcionesI = []
+  for (let i = 0; i < azulejos.length; i++) {
+    opcionesI.push(i);
+  }
+
+  for (let i = 0; i < RETICULA * RETICULA; i++) {
+    celdas[i] = {
+      colapsada: false,
+      opciones: opcionesI
+    }
+  }
+  print(celdas);
 }
 
 function draw() {
-  circle(mouseX, mouseY, 20);
+
 }
